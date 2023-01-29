112ab2ac6a9258ca5a18d23adcc27ff8a1c2df54
JongJae Park <pjj186@naver.com>
2023-01-29 23:33:26 +0900

    paging

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import React from "react";
 
 interface ISectionProps {
   pageNum: number;
   bgColor: string;
+  window: Window;
+  pageRefs: React.MutableRefObject<HTMLDivElement[]>;
 }
 
 const Section = (props: ISectionProps) => {
-  const [windowObj, setWindowObj] = useState<Window>();
-  useEffect(() => {
-    if (window !== undefined) {
-      setWindowObj(window);
-    }
-  }, []);
-
   return (
-    <div className={`w-screen h-screen ${props.bgColor}`}>
+    <div
+      ref={(element) => {
+        props.pageRefs.current[props.pageNum] = element!;
+      }}
+      className={`w-screen h-screen ${props.bgColor}`}
+    >
       <span>Page {props.pageNum}</span>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Section from "@/components/Section";
 import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
 
 const pageObjArrays = [
   { pageNum: 1, bgColor: "bg-[#ffeaa7]" },
@@ -9,6 +10,43 @@ const pageObjArrays = [
 ];
 
 const Home = () => {
+  const [windowObj, setWindowObj] = useState<Window>();
+  const [currentPageNum, setCurrentPageNum] = useState<number>(1);
+  const totalNum = pageObjArrays.length;
+  const pageRefs = useRef<HTMLDivElement[]>([]); // 0번 인덱스 비어있음
+
+  useEffect(() => {
+    if (window !== undefined) {
+      setWindowObj(window);
+    }
+  }, []);
+
+  // 페이지 변경 함수
+  const handlePageChange = (event: Event) => {
+    let scroll = windowObj?.scrollY!;
+    for (let i = 1; i <= totalNum; i++) {
+      console.log(i);
+      // 스크롤이 해당 섹션에 진입했는지 판단 && 해당 스크롤이 해당 섹션에 머물러 있는지
+      if (
+        scroll > pageRefs.current[i].offsetTop - windowObj!.outerHeight / 3 &&
+        scroll <
+          pageRefs.current[i].offsetTop -
+            windowObj!.outerHeight / 3 +
+            pageRefs.current[i].offsetHeight
+      ) {
+        setCurrentPageNum(i);
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    windowObj?.addEventListener("scroll", handlePageChange);
+    return () => {
+      windowObj?.removeEventListener("scroll", handlePageChange);
+    };
+  }, [windowObj]);
+
   return (
     <>
       <Head>
@@ -21,9 +59,14 @@ const Home = () => {
               key={index}
               pageNum={item.pageNum}
               bgColor={item.bgColor}
+              window={windowObj!}
+              pageRefs={pageRefs}
             />
           );
         })}
+        <span className="fixed top-0 right-0 mx-auto">
+          현재 페이지는 {currentPageNum} 입니다.
+        </span>
       </main>
     </>
   );
